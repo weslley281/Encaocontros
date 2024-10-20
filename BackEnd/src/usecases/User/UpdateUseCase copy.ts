f927fe8291e52951afcb895f460f5624c1051b13
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2024-10-20 12:53:04 -0400

    a

---
+++ BackEnd/src/usecases/User/UpdateUseCase copy.ts
@@ -0,0 +1,33 @@
+import { encrypt } from '../../utils/crypto';
+import { User } from '../../models/User';
+import { AppError } from '../../errors/AppErros';
+import { IUserRepository } from '../../repositories/IUserRepository';
+
+interface IRequest {
+  user_id: number;
+  user_type: string;
+  name: string;
+  phone: string;
+  email: string;
+  cpf: string;
+  birthday: Date;
+}
+
+export class UpdateUserUseCase {
+  constructor(private userRepository: IUserRepository) {}
+
+  async execute(data: IRequest): Promise<User> {
+    const existingUser = await this.userRepository.findById(data.user_id);
+
+    if (!existingUser) {
+      throw new AppError('Usuário não existe');
+    }
+
+    const encryptedCPF = encrypt(data.cpf);
+
+    return await this.userRepository.update({
+      ...data,
+      cpf: encryptedCPF,
+    });
+  }
+}
