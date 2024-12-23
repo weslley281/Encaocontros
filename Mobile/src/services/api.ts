15d06702a5cee9f1de269820f7e50cf288477c62
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2024-12-23 09:36:21 -0400

    a

--- Mobile/src/services/api.ts
+++ Mobile/src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-export const ip = 'http://192.168.0.17:5500'
+export const ip = 'http://192.168.0.17:5500';
+// export const ip = "http://192.168.1.100:5500";
+
 export const api = axios.create({
   baseURL: ip,
-
-  //baseURL: "http://192.168.1.100:5500",
 });
